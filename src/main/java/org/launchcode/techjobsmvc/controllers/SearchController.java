56d5c2864d39fdeb436d26c27cf225a1edeee68b
thadtastic <leothaddeus92@gmail.com>
2024-02-11 18:12:48 -0600

    still coding out displaySearchResults method

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,12 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +25,15 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+//    how do I requestparam for a radio button???
+    @PostMapping
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        if(searchTerm.equals("all") || searchTerm.isEmpty()){
+
+        }else{
+
+        }
+    }
 
 }
 
