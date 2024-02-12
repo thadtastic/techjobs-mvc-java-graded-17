1c5a2d944ad1244621d9f4e1b047673af5116c66
thadtastic <leothaddeus92@gmail.com>
2024-02-12 16:32:02 -0600

    created displaySearchResults mehtod in SearchController

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,5 +1,6 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
 import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.print.attribute.standard.JobName;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -25,14 +30,22 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-//    how do I requestparam for a radio button???
-    @PostMapping
+//    how do I requestparam for a radio button??? used the form field names
+    @PostMapping("results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs = null;
         if(searchTerm.equals("all") || searchTerm.isEmpty()){
-
+            //call the findAll() method from JobData, store the results in jobs ArrayList.
+            jobs = JobData.findAll();
         }else{
+            //Otherwise, send the search information to findByColumnAndValue, store the results in jobs ArrayList.
+            model.addAttribute("jobs", jobs);
+            //copied from search method.
+            model.addAttribute("columns", columnChoices);
+
 
         }
+        return "search";
     }
 
 }
